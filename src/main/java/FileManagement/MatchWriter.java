27dd94f49e721fbb352cbc708dcd4a2a83a4ab8c
AdriaValls <avallsmo12@alumnes.ub.edu>
2022-06-09 15:53:47 +0200

    He canviat la classe MatchWriter i MatchReader funcionant amb integers, la classe MatchReader es més una plantilla per quan fem el encoder i decoder que un classe a utilitzar.

--- src/main/java/FileManagement/MatchWriter.java
+++ src/main/java/FileManagement/MatchWriter.java
@@ -3,13 +3,13 @@ package FileManagement;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 //Each
 public class MatchWriter {
+    List<Integer> cellList;
     List<Integer> xList;
     List<Integer> yList;
 
@@ -17,32 +17,34 @@ public class MatchWriter {
 
     //TODO: for decoder, initialize match list from the file
     public MatchWriter()  {
+        cellList = new ArrayList<>();
         xList = new ArrayList<>();
         yList = new ArrayList<>();
     }
 
-    public void addMatch(int xCoord, int yCoord){
+    public void addMatch(int cellNumber, int xCoord, int yCoord){
+        cellList.add(cellNumber);
         xList.add(xCoord);
         yList.add(yCoord);
     }
 
-    public void saveToFile(File file){
-        File matchFile = file;
+    public void saveToFile(File matchFile){
+
         try {
 
             FileOutputStream fos = new FileOutputStream(matchFile, true);
-            String outputString = "";
-            outputString += xList.size();
+
+            fos.write(xList.size());
 
             for(int i=0; i<xList.size(); i++){
-
-                outputString += (xList.get(i).toString());
-                System.out.println("x = "+xList.get(i));
-                outputString += (yList.get(i).toString());
-                System.out.println("y = "+yList.get(i));
+                //Cell number on the destiny image
+                fos.write(cellList.get(i));
+                //X coordinate on the base image
+                fos.write(xList.get(i));
+                //Y coordinate on the base image
+                fos.write(yList.get(i));
             }
-            byte[] b= outputString.getBytes();
-            fos.write(b);
+
             fos.close();
 
         } catch (IOException e) {
--- src/test/java/MatchWriterTest.java
+++ src/test/java/MatchWriterTest.java
@@ -1,6 +1,8 @@
+import FileManagement.MatchReader;
 import FileManagement.MatchWriter;
 import org.junit.Test;
 import java.io.File;  // Import the File class
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -14,10 +16,18 @@ public class MatchWriterTest {
         try{
             fileCreated = matchFile.createNewFile();
             if(fileCreated){
+                FileOutputStream fos = new FileOutputStream(matchFile, true);
+                String outstr = "";
+                int numFrames = 1;
+                outstr += numFrames;
+                byte[] b= outstr.getBytes();
+                fos.write(b);
+                fos.close();
+
                 System.out.print("file Created");
                 MatchWriter match = new MatchWriter();
-                match.addMatch(1,1);
-                match.addMatch(4,5);
+                match.addMatch(1,1 , 2);
+                match.addMatch(4,5, 6);
                 match.saveToFile(matchFile);
             }else{
                 System.out.print("file Created");
@@ -26,20 +36,35 @@ public class MatchWriterTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        /*
-        try {
-            FileWriter myWriter = new FileWriter("C:\\Users\\adriv\\IdeaProjects\\Project_Video_Codec\\MatchFile.txt");
-            MatchWriter match = new MatchWriter();
-            match.addMatch(1,1);
-            match.addMatch(4,5);
-            match.saveToDir(myWriter);
-            myWriter.close();
+    @Test
+    public void fileReadingTest(){
+        String filePath = "C:\\Users\\adriv\\IdeaProjects\\Project_Video_Codec\\MatchFile.txt";
+        File matchFile = new File(filePath);
+        boolean fileCreated;
+        try{
+            fileCreated = matchFile.createNewFile();
+
+            if(fileCreated){
+
+                FileOutputStream fos = new FileOutputStream(matchFile, true);
+                int numFrames = 1;
+                fos.write(numFrames);
+                fos.close();
+
+                System.out.print("file Created");
+                MatchWriter match = new MatchWriter();
+                match.addMatch(1,1 , 2);
+                match.addMatch(4,5, 6);
+                match.saveToFile(matchFile);
+            }else{
+                System.out.print("file Created");
+            }
+            MatchReader reader = new MatchReader(matchFile);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-*/
-
     }
 }
